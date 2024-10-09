5e031d771b1f63b4066dd393b8d9e1e0b9ab47d7
huseyinsr <39071@ma-web.nl>
2024-10-09 12:25:04 +0200

    Add files via upload

---
+++ moetiknaarschool.js
@@ -0,0 +1,13 @@
+const huidigeDag = "maandag";
+
+if (huidigeDag == "maandag" || huidigeDag == "dinsdag" || huidigeDag == "woensdag" || huidigeDag == "donderdag" || huidigeDag == "vrijdag") {
+    console.log("Ik moet naar school");
+} 
+
+else if (huidigeDag == "zaterdag" || huidigeDag == "zondag") {
+    console.log("Lekker uitslapen!");
+} 
+
+else {
+    console.log("Ongeldige dag ingevoerd");
+}
